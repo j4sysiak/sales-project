2e12b522217d60442308d2335999f8f48d4b33ad
Jacek <jaceksysiak@wp.pl>
2020-06-05 16:34:00 +0200

    nn

--- sales-project/src/app/sales-person-list/sales-person-list.component.spec.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SalesPersonListComponent } from './sales-person-list.component';
 
-
 describe('SalesPersonListComponent', () => {
   let component: SalesPersonListComponent;
   let fixture: ComponentFixture<SalesPersonListComponent>;
--- sales-project/src/app/sales-person-list/sales-person.spec.ts
+++ sales-project/src/app/sales-person-list/sales-person.spec.ts
@@ -2,6 +2,6 @@ import { SalesPerson } from './sales-person';
 
 describe('SalesPerson', () => {
   it('should create an instance', () => {
-    expect(new SalesPerson()).toBeTruthy();
+    expect(new SalesPerson("firstName", "lastName", "email", 1)).toBeTruthy();
   });
 });
